2fef7861b29f4d260cb21897cee0de2781e3457f
YuSer <ruslan.skvortsov@iw-group.pro>
2025-12-21 01:08:31 +0400

    Правки

--- src/main/java/me/nobugs/lesson/homeWork2/task10/tenthTaskLaptop/Main.java
+++ src/main/java/me/nobugs/lesson/homeWork2/task10/tenthTaskLaptop/Main.java
@@ -2,13 +2,11 @@ package me.nobugs.lesson.homeWork2.task10.tenthTaskLaptop;
 
 public class Main {
     public static void main(String[] args) {
-        Laptop l = new Laptop("Apple",150000);
+        Laptop l = new Laptop("Apple",150000.00);
         l.printInfo();
 
-        l.setPrice(179999);
+        l.setPrice(179999.00);
         l.printInfo();
-
-
     }
 
 }
--- src/main/java/me/nobugs/lesson/homeWork2/task8/eighthTackTeacher/Main.java
+++ src/main/java/me/nobugs/lesson/homeWork2/task8/eighthTackTeacher/Main.java
@@ -3,7 +3,7 @@ package me.nobugs.lesson.homeWork2.task8.eighthTackTeacher;
 public class Main {
     public static void main(String[] args) {
         Teacher t = new Teacher("Галина","Геометрия");
-
+        t.printInfo();
 
         t.setSubject("Математика");
         t.printInfo();
